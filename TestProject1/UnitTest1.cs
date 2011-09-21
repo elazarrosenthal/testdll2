63ae570f61f9e29679d7f203fcd4748edba61f3c
U-AM\erose <erose@dt-dell4h5vkm1.AM.IDTCORP.NET>
2011-09-21 11:51:40 -0400

    new git instance

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDll;
+
+
+
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestShouldPass()
+        {
+            Assert.IsTrue(UselessStuff.myTrue());
+        }
+
+        [TestMethod]
+        public void TestShouldFail()
+        {
+            Assert.IsTrue(!(UselessStuff.myTrue()));
+        }
+    }
+}
